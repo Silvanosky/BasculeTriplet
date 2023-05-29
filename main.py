571daf364dcfd37796cda52a833bbd648284e32f
Charles Villard <charles.villard@epita.fr>
2023-05-29 17:37:58 +0200

    edit: main: select overlapping triplets

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 import xml.etree.ElementTree as ET
 import sys,os
 import parse
+from scipy.spatial.transform import Rotation as R
+import numpy as np
 
 if len(sys.argv) < 4:
     print(sys.argv[0] + " TRIPLETPATH ORI1 OR2")
@@ -37,6 +39,7 @@ class Triplet():
             i += 1
         return r
 
+
 def xml_loadPos(e):
     pos = [0] * 3
     i = 0
@@ -62,16 +65,14 @@ def loadImages(path):
     with os.scandir(path + '/') as entries:
         for entry in entries:
             if entry.name.startswith("Orientation-"):
-                print(entry.name)
                 root = ET.parse(path + '/' + entry.name).getroot()
+                if "ExportAPERO" == root.tag: #Handle when everything inside
+                    root = root.find("OrientationConique")
 
                 c = root.find('Externe')
                 pos = xml_loadPos(c)
                 rot = xml_loadRot(c.find("ParamRotation").find('CodageMatr'))
                 name = parse.parse("Orientation-{}.xml", entry.name)[0]
-                print(name)
-                print(pos)
-                print(rot)
 
                 images.append(Image(name, pos, rot))
     return images
@@ -82,7 +83,6 @@ def loadTripletList(path):
         for entry in entries:
             if entry.name.startswith("ListeTriplets") and \
                 entry.name.endswith(".xml"):
-                print (entry.name)
                 root = ET.parse(path + '/' + entry.name).getroot()
                 for c in root.findall('Triplets'):
                     names = [c.find("Name1").text, c.find("Name2").text,
@@ -101,13 +101,63 @@ def loadTripletList(path):
                     triplets_list.append(Triplet(names, pos, rot))
     return triplets_list
 
+def checkUnique(images1, images2):
+    names = set()
+    for i in images1:
+        names.add(i.name)
+    for i in images2:
+        if i.name in names:
+            return False
+    return True
+
+
 images1 = loadImages(ORI1_NAME)
 images2 = loadImages(ORI2_NAME)
 
-triplets_list = loadTripletList(TRIP_NAME)
+if not checkUnique(images1, images2):
+    print("Same image in both block")
+    exit(1)
+
+simages1 = set()
+for i in images1:
+    simages1.add(i.name)
+simages2 = set()
+for i in images2:
+    simages2.add(i.name)
+
+triplets_list_all = loadTripletList(TRIP_NAME)
+triplets_list = []
+
+for t in triplets_list_all:
+    in1 = 0
+    in2 = 0
+    mask = [0]*3
+    for i in [0, 1 , 2]:
+        if t.names[i] in simages1:
+            in1 += 1
+            mask[i] = 1
+        if t.names[i] in simages2:
+            in2 += 1
+            mask[i] = 2
+
+    if (not in1) or (not in2) or (in1 + in2 != 3):
+        continue
+
+    direct = in1 == 2
+    selector = 1 if direct else 2
+    m = [0] * 3;
+    for i in [0, 1, 2]:
+        if mask[i] != selector:
+            m[2] = i
+    m[1] = (m[2] + 2) % 3;
+    m[0] = (m[2] + 1) % 3;
+
+    triplets_list.append(t)
 
 for t in triplets_list:
     print(t)
+
+print(len(triplets_list))
 
 
 
